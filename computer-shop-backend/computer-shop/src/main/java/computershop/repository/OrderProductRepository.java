ffded3363cc6b37878c7c6fd6cb8dbb000eac0b6
Konrad <59510358+ressolcez@users.noreply.github.com>
2023-03-03 14:55:18 +0100

    Delete computer-shop-backend directory

--- computer-shop-backend/computer-shop/src/main/java/computershop/repository/OrderProductRepository.java
+++
@@ -1,9 +0,0 @@
-package computershop.repository;
-
-import computershop.model.OrderProductModel;
-import computershop.model.OrdersModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderProductRepository extends JpaRepository<OrderProductModel, Long> {
-
-}
--- computer-shop-backend/computer-shop/src/main/java/computershop/repository/RolesRepository.java
+++
@@ -1,7 +0,0 @@
-package computershop.repository;
-
-import computershop.model.RolesModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolesRepository extends JpaRepository<RolesModel, Long> {
-}
